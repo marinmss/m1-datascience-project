301938e94a0732c57bec3187960d7efae110a974
jaklecrack <musse.marina@orange.fr>
2024-05-23 23:20:39 +0200

    storing wikipedia biography into dataframe with fetchBiography function

--- src/task1.py
+++ src/task1.py
@@ -1,7 +1,36 @@
+import pandas as pd
+import wikipediaapi
+
 ### DATA COLLECTION ##########################################################################
+
+def fetchBiographies():
+    wiki = wikipediaapi.Wikipedia('en')
+
+    data = []
+
+    sculptors = wiki.page("Category:sculptors")
+    articles = sculptors.categorymembers
+
+    for title, page in articles.items():
+            if page.ns == 0: 
+                text = page.text
+                data.append({'text': text, 'category': "Sculptors"})
+
+    df = pd.DataFrame(data)
+    return df
 
 
 ### DATA ANALYSIS ############################################################################
 
 
 ### CLUSTERING ###############################################################################
+
+
+### MAIN #####################################################################################
+
+def main():
+    fetchBiographies()
+
+
+if __name__=='__main__':
+    main()
