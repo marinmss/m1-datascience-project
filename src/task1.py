03bde5553655c8c0661711a020919c6700e7e06a
Abigail <abigail.berthe@gmail.com>
2024-06-04 10:49:27 +0200

    update functions names and docstrings

--- src/task1.py
+++ src/task1.py
@@ -7,46 +7,67 @@ import json
 
 ### DATA COLLECTION ##########################################################################
 
-def fetchBiographies(categorymembers, category, data, number_people, level=0, max_level=1): 
-        """Iterate through each page in the category members to fetch the biographies"""
+def collectBiographiesFromCategories(categorymembers, category, data, number_people, level=0, max_level=1): 
+    """
+    Recursively collects biographical texts and fact graphs for each person in specified Wikipedia category members up to a given recursion level.
 
-        for page in categorymembers.values():
-            if page.ns == 0 and 'List' not in page.title and number_people < 130: #limit the number of people for each category to 130
-    
-                #some wikipedia page cannot be found in dbpedia, we ignore them
-                try: 
+    Parameters:
+        categorymembers (dict): A dictionary of category member pages from the WikipediaAPI.
+        category (str): The name of the category being processed.
+        data (list): A list to store biographical text and associated metadata.
+        number_people (int): Counter for the number of biographies collected.
+        level (int): Current level of recursion through category members.
+        max_level (int): Maximum depth of recursion allowed.
 
-                    #knowledge graph of facts
-                    kg_graph = fetchFacts(page.pageid, page.title)
-                    # Save the graph of facts to a JSON file
-                    with open('knowledge_graph.json', 'a') as json_file:
-                        json.dump(kg_graph, json_file, indent=4)
-                    print(f"{category} {page.title} processed")
+    Returns:
+        int: Updated count of processed biographies.
+    """
 
+    for page in categorymembers.values():
+        if page.ns == 0 and 'List' not in page.title and number_people < 130: #limit the number of people for each category to 130
 
-                    text = page.text
-                    data.append({'text': text, 'category': category})
-                    #creation of txt file for the biography
-                    f = open(f"Biographies_{category}/{(page.title).replace(' ', '')}_{category}.txt", "w")
-                    f.write(text)
-                    f.close()
+            #some wikipedia page cannot be found in dbpedia, we ignore them
+            try: 
 
-                    #keep track of people processed
-                    number_people += 1  
-
-                except Exception as err:
-                    print(f"Unexpected {err=}, {type(err)=}, could not process {page.title}")
-                    continue
+                #knowledge graph of facts
+                kg_graph = fetchDbpediaFacts(page.pageid, page.title)
+                # Save the graph of facts to a JSON file
+                with open('knowledge_graph.json', 'a') as json_file:
+                    json.dump(kg_graph, json_file, indent=4)
+                print(f"{category} {page.title} processed")
 
 
-            if page.ns == wikipediaapi.Namespace.CATEGORY and level < max_level and number_people < 130:
-                # Recursively call the function to process pages in the subcategory 
-                number_people = fetchBiographies(page.categorymembers, category, data, number_people, level=level + 1, max_level=max_level)
-        # return updated count of processed people
-        return number_people
+                text = page.text
+                data.append({'text': text, 'category': category})
+                #creation of txt file for the biography
+                f = open(f"Biographies_{category}/{(page.title).replace(' ', '')}_{category}.txt", "w")
+                f.write(text)
+                f.close()
+
+                #keep track of people processed
+                number_people += 1  
+
+            except Exception as err:
+                print(f"Unexpected {err=}, {type(err)=}, could not process {page.title}")
+                continue
 
 
-def formatData():
+        if page.ns == wikipediaapi.Namespace.CATEGORY and level < max_level and number_people < 130:
+            # Recursively call the function to process pages in the subcategory 
+            number_people = collectBiographiesFromCategories(page.categorymembers, category, data, number_people, level=level + 1, max_level=max_level)
+    # return updated count of processed people
+    return number_people
+
+
+def createBiographyDataFrames():
+    """
+    Collects biographical data from Wikipedia categories 'Sculptors' and 'Journalists', 
+    creates directories for storing these biographies, and formats the collected data into a pandas DataFrame.
+
+    Returns:
+        DataFrame: A pandas DataFrame containing the text and category of each biography collected.
+    """
+
     wiki = wikipediaapi.Wikipedia('Mozilla/5.0', 'en')
 
     data = []
@@ -54,19 +75,28 @@ def formatData():
     sculptors = wiki.page("Category:Sculptors")
     if not os.path.exists('Biographies_Sculptors'):
         os.mkdir('Biographies_Sculptors') #creates a directory to store all sculptors' biographies
-    fetchBiographies(sculptors.categorymembers, "Sculptors", data, 0)
+    collectBiographiesFromCategories(sculptors.categorymembers, "Sculptors", data, 0)
     
 
     journalists = wiki.page("Category:Journalists")
     if not os.path.exists('Biographies_Journalists'):
         os.mkdir('Biographies_Journalists') #creates a directory to store all journalists' biographies
-    fetchBiographies(journalists.categorymembers, "Journalists", data, 0)
+    collectBiographiesFromCategories(journalists.categorymembers, "Journalists", data, 0)
 
     df = pd.DataFrame(data)
     return df
 
-def fetchFacts(wiki_id, page_title):
-    """returns triples of facts about the wikipedia page"""
+def fetchDbpediaFacts(wiki_id, page_title):
+    """
+    Fetches RDF triples (facts) about a specific Wikipedia page from DBpedia using the page's ID.
+
+    Parameters:
+        wiki_id (int): The Wikipedia page ID used to fetch the corresponding DBpedia page.
+        page_title (str): Title of the Wikipedia page.
+
+    Returns:
+        dict: A dictionary containing RDF triples with DBpedia facts about the page, its title and other metadata.
+    """
 
     #initialize the SPARQL wrapper
     sparql = SPARQLWrapper("http://dbpedia.org/sparql/")
@@ -115,7 +145,7 @@ def fetchFacts(wiki_id, page_title):
 ### MAIN #####################################################################################
 
 def main():
-    print(formatData())
+    print(createBiographyDataFrames())
 
 
 if __name__=='__main__':
